75d5782c684a60c8670225e84056ba059b8404c0
Jason Merino <jason.merino@build.com>
2017-08-03 16:52:28 -0700

    WIP - update listeners when realm changes.

--- src/connectRealm.js
+++ src/connectRealm.js
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import pluralize from 'pluralize';
-import camelcase from 'camelcase';
-import hoistNonReactStatic from 'hoist-non-react-statics';
+import PropTypes from "prop-types";
+import React from "react";
+import pluralize from "pluralize";
+import camelcase from "camelcase";
+import hoistNonReactStatic from "hoist-non-react-statics";
 
 export function getResultsName(schema) {
   return pluralize(camelcase(schema), 2);
@@ -11,7 +11,7 @@ export function getResultsName(schema) {
 function connectRealm(WrappedComponent, options) {
   class ConnectedRealmComponent extends React.Component {
     static contextTypes = {
-      reactRealmInstance: PropTypes.object,
+      reactRealmInstance: PropTypes.object
     };
 
     constructor(props, context) {
@@ -20,26 +20,53 @@ function connectRealm(WrappedComponent, options) {
       this.schemaList = options.schemas || [];
       this.results = {};
 
-      this.schemaList.forEach(schema => {
-        this.results[
-          getResultsName(schema)
-        ] = context.reactRealmInstance.objects(schema);
-        this.results[getResultsName(schema)].addListener(this.updateView);
-      });
+      this.addListeners(context);
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+      // do comparison of realm
+      console.log(
+        "current context",
+        JSON.stringify(this.context.reactRealmInstance, null, 2)
+      );
+      console.log(
+        "next context",
+        JSON.stringify(nextContext.reactRealmInstance, null, 2)
+      );
+      if (
+        context.reactRealmInstance.identity !==
+        nextContext.reactRealmInstance.identity
+      ) {
+        this.removeListeners();
+        this.addListeners(nextContext);
+      }
     }
 
     componentWillUnmount() {
+      this.removeListeners();
+    }
+
+    addListeners = context => {
+      this.schemaList.forEach(schema => {
+        const name = getResultsName(schema);
+        this.results[name] = context.reactRealmInstance.objects(schema);
+        this.results[name].addListener(this.updateView);
+      });
+    };
+
+    removeListeners = () => {
       this.schemaList.forEach(schema => {
         this.results[getResultsName(schema)].removeListener(this.updateView);
       });
-    }
+      this.results = {};
+    };
 
     getProps = () => {
-      if (options && typeof options.mapToProps === 'function') {
+      if (options && typeof options.mapToProps === "function") {
         return options.mapToProps(
           this.results,
           this.context.reactRealmInstance,
-          this.props,
+          this.props
         );
       }
       return {};
